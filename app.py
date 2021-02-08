3326b808262a49215c2b178ccf7e3a9e59362bf7
helloworld-OSLO <78739100+helloworld-OSLO@users.noreply.github.com>
2021-02-08 15:01:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello():
-    # wb2 = load_workbook(flask.request.files['file'])
-    # wb2['Feuil1']['A1'] = 'Paul est le boooo'
-    # writer = BytesIO()
-    # wb2.save(writer)
-    # writer.seek(0)
-    # # raiponce = func.HttpResponse(writer.getvalue(), mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-    # return flask.send_file(writer, as_attachment=True,
-    # attachment_filename='file.xslx', mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-    return "Hello World"
+    wb2 = load_workbook(flask.request.files['file'])
+    wb2['Feuil1']['A1'] = 'Paul est le boooo'
+    writer = BytesIO()
+    wb2.save(writer)
+    writer.seek(0)
+    # raiponce = func.HttpResponse(writer.getvalue(), mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    return flask.send_file(writer, as_attachment=True,
+    attachment_filename='file.xslx', mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
