ce8814132d6b80c03116fbacaf04f9214963010d
helloworld-OSLO <78739100+helloworld-OSLO@users.noreply.github.com>
2021-02-08 14:46:03 +0100

    init

---
+++ app.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+import flask
+
+from openpyxl import load_workbook
+from io import BytesIO
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = flask.Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def hello():
+    wb2 = load_workbook(flask.request.files['file'])
+    wb2['Feuil1']['A1'] = 'Paul est le boooo'
+    writer = BytesIO()
+    wb2.save(writer)
+    writer.seek(0)
+    # raiponce = func.HttpResponse(writer.getvalue(), mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    return flask.send_file(writer, as_attachment=True,
+    attachment_filename='file.xslx', mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+
+
+if __name__ == "__main__":
+    # Used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host="localhost", port=8080, debug=True)
